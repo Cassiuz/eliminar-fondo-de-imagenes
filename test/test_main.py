4ac4e9de7407f6d4db07bf4536771d9417f6b7d7
Cassiuz <holydayjean@gmail.com>
2025-04-24 21:40:09 -0500

    enfocado a test

---
+++ test/test_main.py
@@ -0,0 +1,44 @@
+import unittest
+
+from main import read_args, main
+
+
+class TestProgram(unittest.TestCase):
+    def setup(self):
+        print('\n========================')
+    
+    def test_read_arg_no_filepatch(self):
+        print('Executing test: test_reads_arg_no_filepath')
+
+        try:
+            args = ['main.py']
+            expected_output = {}
+            self.assertEqual(read_args(args), expected_output)
+            print("test_read_args_no_filepath: OK")
+        except Exception as e:
+            print("test_read_args_no_filepath: ERROR({0})".format(e))
+
+    def test_read_args_filepath_found(self):
+        print('Executing test: test_reads_arg_filepath_found')
+
+        try:
+            args = ['main.py', '-f', '/path/to/image.png']
+            expected_output = {'filepath': '/path/to/image.png'}
+            self.assertEqual(read_args(args), expected_output)
+            print("test_read_args_filepath_found: OK")
+        except Exception as e:
+            print("test_read_args_filepath_found: ERROR({0})".format(e))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
